3869cbafab8245562998f9c708f3f25b92a69c3e
Yash Shukla <yashshuklaa001@gmail.com>
2025-07-07 00:53:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Palindrome Linked List/palindrome-linked-list.java
@@ -0,0 +1,45 @@
+/* Structure of class Node is
+class Node
+{
+    int data;
+    Node next;
+
+    Node(int d)
+    {
+        data = d;
+        next = null;
+    }
+}*/
+
+class Solution {
+    static Node reverse(Node head){
+        if(head==null) return null;
+        Node curr=head;
+        Node prev=null;
+        while(curr!=null){
+            Node next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        return prev;
+    }
+    static boolean isPalindrome(Node head) {
+        Node slow=head;
+        Node fast=head;
+        while(fast.next!=null && fast.next.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        Node p1 = reverse(slow.next);
+        Node p2 = head;
+        while(p1!=null && p2!=null){
+            if(p1.data!=p2.data){
+                return false;
+            }
+            p1=p1.next;
+            p2=p2.next;
+        }
+        return true;
+    }
+}
