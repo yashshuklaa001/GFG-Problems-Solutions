0936afe0660bd11b774162fefd2be71ce5e2987e
Yash Shukla <yashshuklaa001@gmail.com>
2025-08-12 21:37:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological sort/topological-sort.java
+++ Difficulty: Medium/Topological sort/topological-sort.java
@@ -1,35 +1,35 @@
 class Solution {
     public static ArrayList<Integer> topoSort(int V, int[][] edges) {
-        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-        ArrayList<Integer> result = new ArrayList<>();
-        
-        for(int i=0;i<V;i++){
-            graph.add(new ArrayList<>());
-        }
-        for(int edge[] : edges){
-            int u=edge[0];
-            int v=edge[1];
-            graph.get(u).add(v);
-        }
-        boolean []visited = new boolean[V];
-        Stack<Integer> stack = new Stack<>();
-        for(int i=0;i<V;i++){
-            if(!visited[i]){
-                dfs(i,visited,graph,stack);
-            }
-        }
-        while(!stack.isEmpty()){
+         ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+         boolean visited[] = new boolean[V];
+         Stack<Integer> stack = new Stack<>();
+         ArrayList<Integer> result = new ArrayList<>();
+         
+         for(int i=0;i<V;i++){
+             graph.add(new ArrayList<>());
+         }
+         for(int edge[]: edges){
+             int u = edge[0];
+             int v = edge[1];
+             graph.get(u).add(v);
+         }
+         for(int i=0;i<V;i++){
+             if(!visited[i]){
+                 dfs(graph,i,visited,stack);
+             }
+         }
+         while(!stack.isEmpty()){
             result.add(stack.pop());
         }
         return result;
     }
-    static void dfs(int currentNode, boolean []visited,ArrayList<ArrayList<Integer>> graph,Stack<Integer> stack){
-        visited[currentNode]=true;
-        for(int neighbour:graph.get(currentNode)){
-            if(!visited[neighbour]){
-                dfs(neighbour,visited,graph,stack);
+    public static void dfs(ArrayList<ArrayList<Integer>> graph,int node,boolean visited[],Stack<Integer> stack){
+        visited[node]=true;
+        for(int ele : graph.get(node)){
+            if(!visited[ele]){
+                dfs(graph,ele,visited,stack);
             }
         }
-        stack.push(currentNode);
+        stack.push(node);
     }
 }
