112a27456877604ca71daba0c4300ef31abdec55
Yash Shukla <yashshuklaa001@gmail.com>
2025-08-19 03:12:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Quick Sort/quick-sort.java
+++ Difficulty: Medium/Quick Sort/quick-sort.java
@@ -1,35 +1,36 @@
 class Solution {
-    // Function to sort an array using quick sort algorithm.
-    static void quickSort(int arr[], int low, int high) {
-        if(low<high){
-        int pi = partition(arr,low,high);
-        quickSort(arr,low,pi-1);
-        quickSort(arr,pi+1,high);
+    public void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
         }
-        
     }
 
-    static int partition(int arr[], int low, int high) {
-        int pivot = arr[low];
-        int left = low+1;
-        int right = high;
-        while(left<=right){
-            while(left<=right&&arr[left]<=pivot){
-                left++;
+    private int partition(int[] arr, int low, int high) {
+        int pivot = arr[low];   // pivot value
+        int i = low;
+        int j = high;
+
+        while (i < j) {
+            while (i <= high && arr[i] <= pivot) {
+                i++;
             }
-            while(right>=left&&arr[right]>pivot){
-                right--;
+            while (j >= low && arr[j] > pivot) {
+                j--;
             }
-            if(left<right){
-                int temp = arr[left];
-                arr[left]=arr[right];
-                arr[right]=temp;
+            if (i < j) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
             }
         }
+
+        // place pivot at its correct position
         int temp = arr[low];
-        arr[low]=arr[right];
-        arr[right]=temp;
-        
-        return right;
+        arr[low] = arr[j];
+        arr[j] = temp;
+
+        return j;  // return pivot index
     }
 }
