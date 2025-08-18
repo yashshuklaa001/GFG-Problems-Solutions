6562480e2e4b1f312d7e937b0eb8f4b3dc16efe7
Yash Shukla <yashshuklaa001@gmail.com>
2025-08-19 02:37:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Merge Sort/merge-sort.java
@@ -0,0 +1,36 @@
+class Solution {
+
+    void mergeSort(int arr[], int l, int r) {
+        if(l>=r) return;
+        int mid=(l+r)/2;
+        mergeSort(arr,l,mid);
+        mergeSort(arr,mid+1,r);
+        merge(arr,l,mid,r);
+    }
+    void merge(int arr[],int low,int mid,int high){
+        ArrayList<Integer> list = new ArrayList<>();
+        int left =low;
+        int right=mid+1;
+        while(left<=mid && right<=high){
+            if(arr[left]<=arr[right]){
+                list.add(arr[left]);
+                left++;
+            }
+            else{
+                list.add(arr[right]);
+                right++;
+            }
+        }
+        while(left<=mid){
+            list.add(arr[left]);
+            left++;
+        }
+        while(right<=high){
+            list.add(arr[right]);
+            right++;
+        }
+        for(int i=low;i<=high;i++){
+            arr[i]=list.get(i-low);
+        }
+    }
+}
