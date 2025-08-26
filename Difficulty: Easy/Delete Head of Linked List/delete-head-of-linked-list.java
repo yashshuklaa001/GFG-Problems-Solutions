640546e41c7abb27ef9db44fa6cf01bc843c4f52
Yash Shukla <yashshuklaa001@gmail.com>
2025-08-26 20:41:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Delete Head of Linked List/delete-head-of-linked-list.java
@@ -0,0 +1,20 @@
+/*
+class Node
+{
+    int data;
+    Node next;
+
+    Node(int data)
+    {
+        this.data = data;
+        this.next = next;
+    }
+}
+*/
+
+class Solution {
+    public Node deleteHead(Node head) {
+        head=head.next;
+        return head;
+    }
+}
