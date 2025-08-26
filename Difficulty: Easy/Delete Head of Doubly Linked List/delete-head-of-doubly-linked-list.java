401585b2089c153fd6e1b99c5b3ca1d5671dfd1b
Yash Shukla <yashshuklaa001@gmail.com>
2025-08-26 22:31:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Delete Head of Doubly Linked List/delete-head-of-doubly-linked-list.java
@@ -0,0 +1,23 @@
+// User function Template for Java
+
+/*
+class Node
+{
+    int data;
+    Node next, prev;
+    Node(int data)
+    {
+        this.data = data;
+        this.next = this.prev = null;
+    }
+}
+*/
+
+// Complete the function
+class Solution {
+    public static Node deleteHead(Node head) {
+        head=head.next;
+        head.prev=null;
+        return head;
+    }
+}
