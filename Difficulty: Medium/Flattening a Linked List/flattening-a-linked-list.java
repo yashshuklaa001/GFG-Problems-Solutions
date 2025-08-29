039b379172a1420a117a11f71eb7e36591dbc12d
Yash Shukla <yashshuklaa001@gmail.com>
2025-08-29 23:03:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Flattening a Linked List/flattening-a-linked-list.java
+++ Difficulty: Medium/Flattening a Linked List/flattening-a-linked-list.java
@@ -1,33 +1,41 @@
+/*
+class Node {
+    int data;
+    Node next;
+    Node bottom;
+
+    Node(int x) {
+        data = x;
+        next = null;
+        bottom = null;
+    }
+}
+*/
 class Solution {
-    // Function to flatten a linked list
-    Node flatten(Node root) {
-        // code here
-        if (root == null || root.next == null)
+    public Node flatten(Node root) {
+        if(root==null || root.next==null){
             return root;
-
-        // Recur for list on right
-        root.next = flatten(root.next);
-
-        // Merge current list with flattened right list
-        root = merge(root, root.next);
-
+        }
+        Node mergeHead = flatten(root.next);
+        root = merge(root,mergeHead);
         return root;
     }
-     Node merge(Node a, Node b) {
-        if (a == null) return b;
-        if (b == null) return a;
-
-        Node result;
-
-        if (a.data < b.data) {
-            result = a;
-            result.bottom = merge(a.bottom, b);
-        } else {
-            result = b;
-            result.bottom = merge(a, b.bottom);
+    public Node merge(Node l1,Node l2){
+        if(l1==null) return l2;
+        if(l2==null) return l1;
+        Node dummy = new Node(-1);
+        Node temp = dummy;
+        while(l1!=null && l2!=null){
+            if(l1.data<l2.data){
+                temp.bottom=l1;
+                l1=l1.bottom;
+            }else{
+                temp.bottom=l2;
+                l2=l2.bottom;
+            }
+            temp=temp.bottom;
         }
-
-        result.next = null; // important: unlink 'next'
-        return result;
+        temp.bottom=(l1==null)?l2:l1;
+        return dummy.bottom;
     }
 }
